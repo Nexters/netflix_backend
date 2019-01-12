99951959b666050085aec1d2330b1f3198c57cf1
junk-sound <wom2277@naver.com>
2019-01-12 15:41:31 +0900

    modify accountRepository methos for email

--- application/src/main/java/me/ziok/application/dao/AccountRepository.java
+++ application/src/main/java/me/ziok/application/dao/AccountRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 @Transactional
-public interface AccountRepository extends JpaRepository<Account, Integer> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    Optional<Account> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
