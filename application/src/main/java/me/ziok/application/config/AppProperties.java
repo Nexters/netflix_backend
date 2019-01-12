a65374e858d43410f36fe52047e9ddb438d90139
junk-sound <wom2277@naver.com>
2019-01-12 15:13:34 +0900

    binding appProperties

--- application/src/main/java/me/ziok/application/config/AppProperties.java
+++ application/src/main/java/me/ziok/application/config/AppProperties.java
@@ -1,4 +1,49 @@
 package me.ziok.application.config;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigurationProperties(prefix = "app")
 public class AppProperties {
+
+    private final Auth auth = new Auth();
+    private final OAuth2 oAuth2 = new OAuth2();
+
+    @Getter
+    @Setter
+    public static class Auth{
+        private String tokenSecret;
+        private long tokenExpirationMsec;
+    }
+
+
+    public static final class OAuth2 {
+
+        private List<String> authorizedRedirectUris = new ArrayList<>();
+
+        public List<String> getAuthorizedRedirectUris() {
+            return authorizedRedirectUris;
+        }
+
+        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
+            this.authorizedRedirectUris = authorizedRedirectUris;
+            return this;
+        }
+    }
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public OAuth2 getoAuth2() {
+        return oAuth2;
+    }
+
+
+
 }
