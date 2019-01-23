93fa7551314ac75180d40efd55786251b19ffa2e
junk-sound <wom2277@naver.com>
2019-01-23 17:45:59 +0900

    define payloads

---
+++ application/src/main/java/me/ziok/application/payload/JwtAuthenticationResponse.java
@@ -0,0 +1,20 @@
+package me.ziok.application.payload;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class JwtAuthenticationResponse {
+
+    @NonNull
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+
+
+
+}
--- application/src/main/java/me/ziok/application/payload/SignUpRequest.java
+++ application/src/main/java/me/ziok/application/payload/SignUpRequest.java
@@ -1,15 +1,27 @@
 package me.ziok.application.payload;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
+@Getter
+@Setter
 public class SignUpRequest {
 
     @NotBlank
+    @Size(min = 4, max = 40)
     private String name;
 
     @NotBlank
+    @Size(min = 3, max = 15)
+    private String nickName;
+
+    @NotBlank
     @Email
+    @Size(max = 40)
     private String email;
 
     @NotBlank
